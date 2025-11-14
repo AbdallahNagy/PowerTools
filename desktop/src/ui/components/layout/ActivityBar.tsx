1453df6d74ca89e08e83717d4a3affe23a284e7c
abdallah nagy <abdallahnagy773@gmail.com>
2025-11-14 02:42:39 +0200

    add icons

--- desktop/src/ui/components/layout/ActivityBar.tsx
+++ desktop/src/ui/components/layout/ActivityBar.tsx
@@ -1,3 +1,7 @@
+import ConnectIcon from '../../assets/icons/connect-icon.svg';
+import DataMigrationIcon from '../../assets/icons/data-migration-icon.svg';
+import PluginRegistrationIcon from '../../assets/icons/plugin-icon.svg';
+
 const ActivityBar = () => {
   return (
     <div className="w-12 bg-[#252526] flex flex-col items-center py-2 select-none">
@@ -6,7 +10,7 @@ const ActivityBar = () => {
         aria-label="connect"
         className="w-10 h-10 flex items-center justify-center text-gray-400 hover:text-white focus:outline-none mb-2"
       >
-        cnct
+        <img src={ConnectIcon} alt="" className='w-full h-full object-cover'/>
       </button>
 
       <button
@@ -14,7 +18,7 @@ const ActivityBar = () => {
         aria-label="Data Migration"
         className="w-10 h-10 flex items-center justify-center text-gray-400 hover:text-white focus:outline-none mb-2"
       >
-        DM
+        <img src={DataMigrationIcon} alt="" />
       </button>
 
       <button
@@ -22,7 +26,7 @@ const ActivityBar = () => {
         aria-label="Plugin Registration"
         className="w-10 h-10 flex items-center justify-center text-gray-400 hover:text-white focus:outline-none"
       >
-        PR
+        <img src={PluginRegistrationIcon} alt="" />
       </button>
     </div>
   );
