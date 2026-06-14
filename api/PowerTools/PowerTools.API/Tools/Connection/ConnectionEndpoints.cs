80077fe6a5f3039c8205a80b2bd9b1c67632d91b
abdallah nagy <abdallahnagy773@gmail.com>
2026-06-15 00:47:16 +0300

    chores: actually try connecting to environment using who am i when connecting

--- api/PowerTools/PowerTools.API/Tools/Connection/ConnectionEndpoints.cs
+++ api/PowerTools/PowerTools.API/Tools/Connection/ConnectionEndpoints.cs
@@ -1,4 +1,6 @@
+using Microsoft.Crm.Sdk.Messages;
 using PowerTools.API.Filters;
+using PowerTools.API.Services;
 using PowerTools.API.Utils;
 
 namespace PowerTools.API.Tools.Connection;
@@ -11,9 +13,23 @@ public static class ConnectionEndpoints
             .RequireAuthorization()
             .AddEndpointFilter<DataverseContextFilter>();
 
-        group.MapGet("/connect", (HttpContext ctx) =>
-            Results.Ok(new { connected = true, environment = ctx.GetEnvironmentUrl() })
-        ).WithName("Connect");
+        group.MapGet("/connect",
+            (HttpContext ctx, DataverseClientFactory factory) =>
+            {
+                try
+                {
+                    var svc = ctx.CreateDataverseClient(factory);
+                    var who = (WhoAmIResponse)svc.Execute(new WhoAmIRequest());
+                    return Results.Ok(new
+                        { connected = true, environment = ctx.GetEnvironmentUrl(), userId = who.UserId });
+                }
+                catch (Exception ex)
+                {
+                    return Results.Json(
+                        new { connected = false, environment = ctx.GetEnvironmentUrl(), error = ex.Message },
+                        statusCode: StatusCodes.Status502BadGateway);
+                }
+            }).WithName("Connect");
 
         return app;
     }
