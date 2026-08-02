62cf8d28f34bdae7579ea98923cfecc204f2186c
abdallah nagy <abdallahnagy773@gmail.com>
2026-08-02 03:56:12 +0300

    publish-website-with-github-pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
-  site: "https://powertools.dev"
+  site: isGitHubPages
+    ? "https://abdallahnagy.github.io"
+    : "https://powertools.dev",
+  base: isGitHubPages ? "/PowerTools" : "/"
 });
